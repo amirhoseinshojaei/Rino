ed78a6b17b9340baa7f5afa997bd1ce93ef16fad
AmirhoseinShojaei <127549836+amirhoseinshojaei@users.noreply.github.com>
2024-11-22 00:20:00 +0330

    build custome user manager model

--- rino/core/models.py
+++ rino/core/models.py
@@ -1,3 +1,43 @@
 from django.db import models
-
+import datetime
+from django.utils import timezone
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin
 # Create your models here.
+
+
+class UserManager(BaseUserManager):
+    def create_user(self, first_name, last_name, email, password=None):
+        if not first_name:
+            raise ValueError('نام خود را وارد کنید')
+
+        if not last_name:
+            raise ValueError('نام خانوادگی خودرا وارد کنید')
+
+        if not email:
+            raise ValueError('ایمیل خود را وارد کنید')
+
+        user = self.model(
+            first_name=first_name,
+            last_name=last_name,
+            email=self.normalize_email(email),
+        )
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    # SuperUser section
+    def create_superuser(self, first_name, last_name, email, password):
+        user = self.create_user(
+            first_name=first_name,
+            last_name=last_name,
+            email=email,
+            password=password,
+        )
+        user.is_admin = True
+        user.is_staff = True
+        user.is_superuser = True
+        user.save(using=self._db)
+        return user
+
+
+
